cc8e0eeb6c417558907462244d20c4d72ced8c7c
Brenda Barraza <74740129+brendabarraza@users.noreply.github.com>
2022-01-19 19:57:42 -0300

    Create API1.go

---
+++ API1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprintf(w, "method not allowed")
+			return
+		}
+
+		fmt.Fprintf(w, "hello world %s", "visitor")
+	})
+
+	srv := http.Server{
+		Addr: ":8080",
+	}
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
