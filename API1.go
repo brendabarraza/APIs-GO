738e3a8949abd8ebce8e79e3375600ab118ea196
Brenda Barraza <74740129+brendabarraza@users.noreply.github.com>
2022-01-20 22:32:41 -0300

    Update API1.go

--- API1.go
+++ API1.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"net/http"
 )
-
 func main() {
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
 			w.WriteHeader(http.StatusMethodNotAllowed)
@@ -16,11 +14,9 @@ func main() {
 
 		fmt.Fprintf(w, "hello world")
 	})
-
 	srv := http.Server{
 		Addr: ":8080",
 	}
-
 	err := srv.ListenAndServe()
 
 	if err != nil {
