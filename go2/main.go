7f1a7960ad724f39d1693456226aa9adf93c2105
Brenda Barraza <74740129+brendabarraza@users.noreply.github.com>
2022-03-07 22:17:52 -0300

    Add files via upload

---
+++ go2/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"tidy/server"
+)
+
+func main() {
+	ctx := context.Background()
+	serverDoneChan := make(chan os.Signal, 1)
+	signal.Notify(serverDoneChan, os.Interrupt, syscall.SIGTERM)
+	srv := server.New(":8080")
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	log.Println("server started")
+	<-serverDoneChan
+	srv.Shutdown(ctx)
+	log.Println("server stopped")
+}
---
+++ go2/server/handlers.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "method not allowed")
+		return
+	}
+	fmt.Fprintf(w, "hello %s", "visitor")
+}
+
+func getCountries(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(countries)
+}
+
+func addCountries(w http.ResponseWriter, r *http.Request) {
+	country := &Country{}
+	err := json.NewDecoder(r.Body).Decode(country)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+	countries = append(countries, country)
+	fmt.Fprintf(w, "country was added")
+}
